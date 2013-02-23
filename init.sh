cb5bb0759ce8c9a7631fcb2040f1a4de5adcb098
Luca Clementi <luca.clementi@gmail.com>
2013-02-22 20:25:03 -0800

    Track execution time
    Signed-off-by: Luca Clementi <luca.clementi@gmail.com>

--- init.sh
+++ init.sh
@@ -10,14 +10,17 @@ SubModule=`cat .gitignore`
 remote=`git remote -v|awk '{print $2}'|head -n 1`
 baseRemote=`dirname $remote`
 
-pushd src/roll
+start_time=$(date +%s)
 
+pushd src/roll
 for i in $SubModule;
 do 
     modName=`basename $i`
     echo "  Cloning $baseRemote/$modName.git repository" 
     git clone $baseRemote/$modName.git $modName
 done
-
 popd
+
+finish_time=$(date +%s)
+echo "Time duration: $((finish_time - start_time)) secs."
 
